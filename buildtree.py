f3bd5a2864758f21bd3468dad817e5cc683d9d3c
Prasoon04 <pgautam0411@gmail.com>
2022-03-20 15:29:13 -0700

    Code clean up.

--- buildtree.py
+++ buildtree.py
@@ -12,55 +12,60 @@ class Node:
 
 class MerkleTree:
     def __init__(self, inputs) -> None:
-        
         leaves = []
-        
         for element in inputs:
             hashed = self.hash(element)
             leaves.append(Node(None,None,hashed,element))
-        
-        if len(leaves) % 2 != 0:
-            leaves.append(leaves[-1])
-
-        self.root = (self.buildTree(leaves))
-        
+        self.root = self.buildTree(leaves)
+    
     def buildTree(self, nodes)->Node:
-        
-        mid = len(nodes)//2
-
-        if len(nodes) == 2:
-            return Node(nodes[0], nodes[1], self.hash(nodes[0].val+nodes[1].val), nodes[0].data+'|'+nodes[1].data)
-
-        leftnode = self.buildTree(nodes[:mid])
-        rightnode = self.buildTree(nodes[mid:])
-        value = self.hash(leftnode.val + rightnode.val)
-        data = leftnode.data+'|'+rightnode.data
-        return Node(leftnode, rightnode, value, data)
+        while len(nodes) != 1:
+            curr = []
+            for i in range(0, len(nodes), 2):
+                if i == len(nodes)-1:
+                    curr.append(nodes[i])
+                    break
+                else:
+                    left_node = nodes[i]
+                    right_node = nodes[i+1]
+                concathash = self.hash(left_node.val + right_node.val)
+                ancestor = Node(None, None, concathash, left_node.val+' | '+right_node.val)
+                ancestor.left = left_node
+                ancestor.right = right_node
+                curr.append(ancestor)
+            nodes = curr
+        return nodes[0]       
 
     def hash(self, val)->str:
         return hashlib.sha256(val.encode('utf-8')).hexdigest()
 
+    leaves = []
+    def leafnodes(self)->None:
+        temp = []
+        temp.append(self.root)
+        while temp:
+            curr = temp.pop()
+            if curr.left:
+                temp.append(curr.left)
+            if curr.right:
+                temp.append(curr.right)
+            elif not curr.left and not curr.right:
+                self.leaves.append(curr.val)
+        self.leaves.reverse()
     
     def printTree(self)->None:
         self.Treeout(self.root, 0)
 
     stack =[]
-    json_stack = []
 
     def fileOut(self)->None:
         file = open('merkle.tree.txt', 'w+')
         for line in self.stack:
             file.write(line+"\n")
-        self.Inorder(self.root)
-        with open('example.json','w') as outfile:
-            json.dump(self.json_stack, outfile)
-
-    def Inorder(self,node)->None:
-        if node != None:
-            self.Inorder(node.left)
-            self.json_stack.append(["Hash Value: "+str(node.val), "Input: "+str(node.data)])
-            self.Inorder(node.right)
-
+        self.leafnodes()
+        self.leaves = [self.root.val] + self.leaves
+        with open('file1.json', 'w') as leavesfile:
+            json.dump(self.leaves, leavesfile)
 
     def Treeout(self, node, order)->None:
         if node != None:            
