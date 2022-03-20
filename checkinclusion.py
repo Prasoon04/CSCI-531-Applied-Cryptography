8692a864d07290969053f9388728251f5517c0aa
Prasoon04 <pgautam0411@gmail.com>
2022-03-20 15:30:35 -0700

    Code clean up.

--- checkinclusion.py
+++ checkinclusion.py
@@ -6,26 +6,21 @@ import hashlib
 
 class CheckInclusion:
     def __init__(self, Root, Leaves, input) -> None:
-
         print(self.Inclusion(Root, Leaves, input))
 
     def Inclusion(self, Root, Leaves, input)->None:
-
         input_hash = hashlib.sha256(input.encode('utf-8')).hexdigest()
         if input_hash not in Leaves:
             return 'No'
         else:
             output = self.Check_Merkle(Root, Leaves, input_hash)
-        
         if output == 'No':
             return output
         else:
             return ('Yes', output)
 
     def Check_Merkle(self, root_hash, proofs, input_hash)->None:
-
         result = []
-
         while len(proofs) != 1:
             index_val = proofs.index(input_hash)
             if index_val % 2 == 0:
@@ -36,7 +31,6 @@ class CheckInclusion:
                 result.append(proofs[index_val-1])
                 concat = proofs[index_val-1] + input_hash
                 input_hash = hashlib.sha256(concat.encode('utf-8')).hexdigest()
-            
             curr = []
             for i in range(0, len(proofs), 2):
                 if i == len(proofs)-1:
@@ -45,13 +39,10 @@ class CheckInclusion:
                 else:
                     left = proofs[i]
                     right = proofs[i+1]
-                
                 concat = left+right
-                
                 concathash = hashlib.sha256(concat.encode('utf-8')).hexdigest()
                 curr.append(concathash)
             proofs = curr
-        
         if proofs[0] == root_hash:
             return result
         else:
